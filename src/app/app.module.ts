5f5bc0a419d3c3d6749abf28b94da87d891c8741
David Santo <david.santo@hotmail.com.br>
2023-10-02 16:06:28 -0300

    components createds with styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { MenuTittleComponent } from './components/menu-tittle/menu-tittle.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    SmallCardComponent,
+    BigCardComponent,
+    MenuTittleComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
